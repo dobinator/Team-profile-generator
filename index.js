78908e47371939cf0f3dc4c899eae6fc99b20dde
grey cashmere <dobsonemily@gmail.com>
2021-03-20 22:36:55 -0700

    adding code to html and css

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateHTML = (answers)
+const generateHTML = answers;
+
+ const Employee = require("./lib/Employee.js")
+ const Manager = require ("./lib/Manager.js")
+ const Engineer = require ("./lib/Engineer.js")
+ const Intern = require ("./lib/Intern.js")
+
+
+
+
+ 
+const Questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is the employee's role",
+    choices:["Manager", "Engineer", "Intern"], 
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the employee's email address",
+  },
+  {
+    type: "input",
+    name: "userName",
+    message: "What is the Github user name?"
+  }
+];
