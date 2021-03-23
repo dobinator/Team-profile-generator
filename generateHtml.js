c3f3dd55265e8fb582fbeb1e14d4c9da43d578b5
grey cashmere <dobsonemily@gmail.com>
2021-03-23 09:24:27 -0700

    added functions to index, corrected errord

--- generateHtml.js
+++ generateHtml.js
@@ -1,29 +1,8 @@
-//create a function to filter the correct questions
-
-
-
-
-
-//create a function that 
-
-
-
-
-
-
-
 
 //create a function to generate HTML
-function generateHTML(data){
+// function generateHTML(data){
+// }
 
-
-
-
-
-
-
-
-}
 
 
 
--- index.js
+++ index.js
@@ -1,49 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML= require("./generateHTML.js"); 
-const mockup = require("./src/mockup.html");
-const styles = require("./src/styles.css"); 
+const generateHTML = require("./generateHTML.js");
+// const mockup = require("./src/mockup.html");
+// const styles = require("./src/styles.css");
 
+const employees = [];
 
-const employees = []
-
-const Employee = require("./lib/Employee.js");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 
 const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the employee's name?",
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "What is the employee's role",
-    choices: ["Manager", "Engineer", "Intern"],
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the employee's email address",
-  },
-  {
-    type: "input",
-    name: "officeNum",
-    message: "What is the office number?",
-  },
-  {
-    type: "input",
-    name: "userName",
-    message: "What is the Github user name?",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What is the name of the school?",
-  },
   {
     type: "list",
     name: "team",
@@ -52,23 +19,114 @@ const questions = [
   },
 ];
 
+function askQuestion() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then((data) => {
+      if (data.role === "Manager") {
+        addManager();
+      } else if (data.role === "Engineer") {
+        addEngineer();
+      } else if (data.role === "Intern") {
+        addIntern();
+      } else {
+        init();
+      }
+    });
+}
+
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?",
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        message: "What is the office number?",
+      },
+    ])
+    .then((data) => {
+      const manager = new Manager(
+        data.name,
+        data.id,
+        data.email,
+        data.officeNum
+      );
+      employees.push(manager);
+      console.log(employees);
+      askQuestion();
+    });
+}
+askQuestion();
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the Github user name?",
+      },
+    ])
+    .then((data) => {
+      const engineer = new Engineer(data.gitHub);
+      employees.push(engineer);
+      console.log(engineer);
+      askQuestion();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "school",
+        message: "What is the name of the school?",
+      },
+    ])
+    .then((data) => {
+      const intern = new Intern(data.school);
+      emoployees.push(intern);
+      console.log(intern);
+      askQuestion();
+    });
+}
+
 // TODO: Create a function to write HTML file
 function writeToFile(fileName, data) {
-  return fs.writeFile(fileName,data, (err) => err ? console.log
-  (err):  console.log ("website created"))    
-  }; 
-
-
-
-
+  return fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("website created")
+  );
+}
 
 // TODO: Create a function to initialize app
 
-function init()
-inquirer.prompt(questions)
-.then(data => {
-writeToFile ("team.html", generateHTML(data))
+// function init()
+// inquirer.prompt(questions)
+// .then(data => {
+// writeToFile ("team.html", generateHTML(data))
 
-})
-init()
-
+// })
+// init()
