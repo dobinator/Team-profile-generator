f010eb9b3747aa874424d65e0c3df5cc2269860d
grey cashmere <dobsonemily@gmail.com>
2021-03-22 18:46:56 -0700

    updating tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,21 +1,53 @@
-const Employee = require("..lib/Employee");
+const Employee = require("./lib/Employee");
 
 describe("Employee", () => {
   describe ("Initialization", ()=> {
-      text ("Can initalizie an employee object", ()=>{
+      text ("Can initalize an employee object", ()=>{
           const e = new Employee();
           expect(typeof e). toBe("object");
       });
       test ("Can set name attribute with constructor", ()=>{
-          
-      })
-  })
+          const name ="Clark";
+          const e = new Employee (name); 
 
+          expect(e.name). toBe(name);
+      });
 
-})
+      test("can set attribute with constructor", ()=> {
+       const id = 77;
+       const e = new Employee("Nick", id); 
+
+       expect (e.id).toBe(id); 
+
+      }); 
+
+      test("Can set email attribute with constructor", ()=> {
+       const email= "dobsonemily@gmail.com";
+       const e = new Employee("Nick", 77, email);
+
+       expect (e.email).toBe(email);
+
+      });
+  }); 
+}); 
 
 describe("getters", ()=> {
-    test("can get name via getName() method", ()=> {
-        
-    })
-})
+    test("Can get name via getName() method", ()=> {
+        const name ="Nick";
+        const e = new Employee(name, 77, "dobsonemily@gmail.com");
+
+        expect(e. getName()).toBe(name);
+    });
+    test("can get id via getId() method", ()=> {
+     const email = "dobsonemily@gmail.com";
+     const e = new Employee ("Nick", 77, email); 
+
+     expect(e.getEmail()).toBe(email);
+    });
+    test("Can get role via getRole() method", ()=> {
+        const e= new Employee("Nick", 77, "dobsonemily@gmail.com");
+
+        expect(e.getRole()).toBe("Employee");
+    });
+
+});
