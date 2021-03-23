57369de2bf90e1d355f7ec6a55275144f76ed89c
grey cashmere <dobsonemily@gmail.com>
2021-03-23 15:08:28 -0700

    updating tests

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,4 +1,4 @@
-const Intern= require("./lib/Intern.js");
+const Intern= require("./lib/Intern");
 
 describe ("Intern", ()=> {
     describe ("Initialization", ()=> {
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,4 +1,4 @@
-const Manager = require("./lib/Manager.js")
+const Manager = require("./lib/Manager")
 
 
 
