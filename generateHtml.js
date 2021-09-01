63fd3c556818e9a372e1bab2b72f205732ef100f
grey cashmere <dobsonemily@gmail.com>
2021-08-31 22:15:33 -0700

    updating the css and html

--- generateHtml.js
+++ generateHtml.js
@@ -9,75 +9,75 @@ const header = `
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/041ef3023d.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
     <link rel= "stylesheet" href= "styles.css">
 
 </head>
 <body>
-    <main class="container-fluid">
-    <div class = "row">
-    <div class="team-area header col-12 jumbotron mb-3"></div> 
-    <h1 class ="text-center">My Team</h1>
-    </div>`;
+<nav class="navbar">
+    <div class="navbar-header">
+        <span class="navbar-brand mb-0 h1">My Team</span>
+    </div>
+</nav>`;
 
 // generateHTML()
 
 function createManager(data) {
-  return ` <div class= "container">
-        <div class ="row">
-    <div class ="team-area col-12 d-flex justify-content-center"></div>
-    <div class ="card employee-card">
-        <div class= "card-header">
-    <h2 class ="card-title">${data.name} </h2>
-    <h4 class = "card-subtitle mb-2 text">${data.getRole()}</h4>
-    </div>
-    <div class= "card-body">
-    <ul class= "list-group">
-        <li class= "list-group-item">Employee ID: ${data.id}</li>
-        <li class ="list-group-item">Email: <a href=mailto:${data.email}><${data.email}></a></li>
-        <li class= "list-group-item">Office: ${data.officeNum}</li>
-    </ul>
-    </div>
-    </div>`;
+  return `  <main class="container">
+  <div class="row">
+     <div class="card-col" style="width: 18rem">
+     <div class="card-body card-header">
+         <h5 class ="card-title">${data.name}</h5>
+         <h5 class ="card-title"><i class="fa-solid fa-mug"></i></h5>
+         <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
+     </div>
+     <ul class= "list-group list-group-flush">
+         <li class="list-group-item">Employee ID: ${data.id}></li>
+         <li class="list-group-item">Email:<a href=mailto:${data.email}></a></li>
+         <li class="list-group-item">Office: ${data.officeNum}</li>
+     </ul>
+  </div>`;
 }
 
 function createEngineer(data) {
   return `
-    <div class ="card employee-card">
-        <div class= "card-header">
-    <h2 class ="card-title">${data.name}</h2>
-    <h4 class = "card-subtitle mb-2 text">${data.getRole()}</h4>
-    </div>
-    <div class= "card-body">
-    <ul class= "list-group">
-        <li class= "list-group-item">Employee ID: ${data.id}</li>
-        <li class ="list-group-item">Email: <a href=mailto:${data.email}>${data.email}</a></li>
-        <li class= "list-group-item">Github:  <a href=http://github.com/${data.gitHub}>${data.gitHub}</a></li>
-    </ul>
-    </div>
+  <div class="card-col" style="width: 18rem">
+  <div class="card-body card-header">
+      <h5 class ="card-title">${data.name}</h5>
+      <h5 class ="card-title"><i class="fa-solid fa-glasses"></i></h5>
+      <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
+  </div>
+  <ul class= "list-group list-group-flush">
+      <li class="list-group-item">Employee ID:${data.id} </li>
+      <li class="list-group-item">Email: <a href=mailto:${data.email}></a></li>
+      <li class="list-group-item">Github: <a href ='https://www.github.com/${data.gitHub}'>/li>
+  </ul>
+</div>
   `};
 
 function createIntern(data) {
   return ` 
-    <div class ="card employee-card">
-        <div class= "card-header">
-    <h2 class ="card-title">${data.name}</h2>
-    <h4 class = "card-subtitle mb-2 text">${data.getRole()}</h4>
-    </div>
-    <div class= "card-body">
-    <ul class= "list-group">
-        <li class= "list-group-item">Employee ID: ${data.id}</li>
-        <li class ="list-group-item">Email: <a href=mailto:${data.email}>${data.email}</a></li>
-        <li class= "list-group-item">School: ${data.school}</li>
-    </ul>
-    </div>
+  <div class="card-col" style="width: 18rem">
+  <div class="card-body card-header">
+      <h5 class ="card-title">${data.name}</h5>
+      <h5 class ="card-title"><i class="fa-solid fa-graduation-cap"></i></h5>
+      <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
+  </div>
+  <ul class= "list-group list-group-flush">
+      <li class="list-group-item">Employee ID:${data.id}</li>
+      <li class="list-group-item">School Email: <a href=mailto:${data.email} ></a></li>
+      <li class="list-group-item">School: ${data.school} </li>
+  </ul>
+</div>
 `;
 }
 
 const footer = `
+</div>
 </main> 
 </body>
 </html>`;
