31fd1a4077ff187b77a2ac6e4bd30586d87448ef
Emily Dobson <77806090+dobinator@users.noreply.github.com>
2021-09-02 15:10:58 -0700

    swtiched to bulma, cleaned up  code

--- generateHtml.js
+++ generateHtml.js
@@ -37,8 +37,11 @@ function createManager(data) {
      </div>
      <ul class= "list-group list-group-flush">
          <li class="list-group-item">Employee ID: ${data.id}></li>
-         <li class="list-group-item">Email:<a href=mailto:${data.email}></a></li>
-         <li class="list-group-item">Office: ${data.officeNum}</li>
+         <li class="list-group-item">Email:
+         <a href=mailto:${data.email}></a>
+         </li><br>
+         <li class="list-group-item">
+         Office: ${data.officeNum}</li>
      </ul>
   </div>`;
 }
