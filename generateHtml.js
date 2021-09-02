c8751dcf04302e59018e19b35cc9aae28d31e13d
Emily Dobson <77806090+dobinator@users.noreply.github.com>
2021-09-02 15:41:52 -0700

    sorted out the links!!

--- generateHtml.js
+++ generateHtml.js
@@ -3,84 +3,91 @@
 
 const header = `
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <script src="https://use.fontawesome.com/041ef3023d.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/84186745d3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel= "stylesheet" href= "styles.css">
-
 </head>
 <body>
-<nav class="navbar">
-    <div class="navbar-header">
-        <span class="navbar-brand mb-0 h1">My Team</span>
-    </div>
-</nav>`;
+    
+    <section class="section has-background-danger">
+        <h1 class="title has-text-centered has-text-white is-size-2 ">My Team</h1>   
+      </section>
+`;
 
 // generateHTML()
 
 function createManager(data) {
-  return `  <main class="container">
-  <div class="row">
-     <div class="card-col" style="width: 18rem">
-     <div class="card-body card-header">
-         <h5 class ="card-title">${data.name}</h5>
-         <h5 class ="card-title"><i class="fa-solid fa-mug"></i></h5>
-         <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
-     </div>
-     <ul class= "list-group list-group-flush">
-         <li class="list-group-item">Employee ID: ${data.id}></li>
-         <li class="list-group-item">Email:
-         <a href=mailto:${data.email}></a>
-         </li><br>
-         <li class="list-group-item">
-         Office: ${data.officeNum}</li>
-     </ul>
-  </div>`;
+  return `<section>
+  <div class= "columns">
+      <div  class="column">  
+       <div class="card">
+       <div class="card-content">
+         <p class="card-title is-size-4 has-background-link has-text-centered has-text-white">${data.name}</p>
+         <p class="name subtitle is-5 has-text-link has-text-centered"><i class="fas fa-mug-hot"></i></p>
+         <p class="name subtitle is-5 has-background-link has-text-white has-text-centered">${data.getRole()}</p>
+           <div class="content">
+               <p class= "">Employee ID:${data.id}</p>
+               <p class= "">Email:<a href="mailto:${data.email}">${data.email}</a></p>
+               <p class= "">Office Num:${data.officeNum}</p>
+               <br>
+           </div>
+       </div>    
+</div>
+</div>
+  `;
 }
 
 function createEngineer(data) {
   return `
-  <div class="card-col" style="width: 18rem">
-  <div class="card-body card-header">
-      <h5 class ="card-title">${data.name}</h5>
-      <h5 class ="card-title"><i class="fa-solid fa-glasses"></i></h5>
-      <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
-  </div>
-  <ul class= "list-group list-group-flush">
-      <li class="list-group-item">Employee ID:${data.id} </li>
-      <li class="list-group-item">Email: <a href=mailto:${data.email}></a></li>
-      <li class="list-group-item">Github: <a href ='https://www.github.com/${data.gitHub}'>/li>
-  </ul>
+
+  <div class="column">  
+  <div class="card">
+  <div class="card-content">
+    <p class="card-title is-size-4 has-background-link has-text-centered has-text-white">${data.name}</p>
+    <p class="name subtitle is-5 has-text-link has-text-centered"><i class="fas fa-glasses"></i></p>
+    <p class="name subtitle is-5 has-background-link has-text-white has-text-centered">${data.getRole()}</p>
+      <div class="content">
+          <p class= "">Employee ID: ${data.id}</p>
+          <p class= "">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+          <p class= "">Github: <a href="https://www.github.com/${data.gitHub}">${data.gitHub}</a></p>
+          <br>
+      </div>
+  </div>    
+</div>
 </div>
   `};
 
 function createIntern(data) {
   return ` 
-  <div class="card-col" style="width: 18rem">
-  <div class="card-body card-header">
-      <h5 class ="card-title">${data.name}</h5>
-      <h5 class ="card-title"><i class="fa-solid fa-graduation-cap"></i></h5>
-      <h6 class ="card-subtitle mb-2 text-muted">${data.getRole()}</h6>        
+  <div  class="column">  
+  <div class="card">
+  <div class="card-content">
+    <p class="card-title is-size-4 has-background-link has-text-centered has-text-white">${data.name}</p>
+    <p class="name subtitle is-5 has-text-link has-text-centered"><i class="fas fa-graduation-cap"></i></i></p>
+    <p class="name subtitle is-5 has-background-link has-text-white has-text-centered">${data.getRole()}</p>
+          <div class="content">
+              <p class= "">Employee ID: ${data.id}</p>
+              <p class= "">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+              <p class= "">School:${data.school}</a></p>
+              <br>
+          </div>
+      </div>    
   </div>
-  <ul class= "list-group list-group-flush">
-      <li class="list-group-item">Employee ID:${data.id}</li>
-      <li class="list-group-item">School Email: <a href=mailto:${data.email} ></a></li>
-      <li class="list-group-item">School: ${data.school} </li>
-  </ul>
+</div>
+</div>
 </div>
 `;
 }
 
 const footer = `
 </div>
+</section> 
 </main> 
 </body>
 </html>`;
